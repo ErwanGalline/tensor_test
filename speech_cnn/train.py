77bf2763cbcb51da27b0a41238192a88c671d9d0
Erwan <erwan.galline@siclo-mobile.com>
2017-10-05 16:44:00 +0700

    Base files

---
+++ speech_cnn/train.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os.path
+import sys
+
+import numpy as np
+import tflearn as tfl
+import data_loader as data
+
+import input_data
+import models
