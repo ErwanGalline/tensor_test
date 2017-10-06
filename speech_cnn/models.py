e56f189e09e4663c56005512a60228be6d151a55
Erwan <erwan.galline@siclo-mobile.com>
2017-10-06 12:03:21 +0700

    Add files

--- speech_cnn/models.py
+++ speech_cnn/models.py
@@ -2,24 +2,30 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import tflearn as tfl
+import tflearn
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.estimator import regression
+
 
 # Create model
-# Param : dropout value , learning rate
-def create_model(dropout,learning_rate ) :
-    network = tfl.input_data(shape=[None, 28, 28, 1], name='input')
-    network = tfl.conv_2d(network, 32, 3, activation='relu', regularizer="L2")
-    network = tfl.max_pool_2d(network, 2)
-    network = tfl.local_response_normalization(network)
-    network = tfl.conv_2d(network, 64, 3, activation='relu', regularizer="L2")
-    network = tfl.max_pool_2d(network, 2)
-    network = tfl.local_response_normalization(network)
-    network = tfl.fully_connected(network, 128, activation='tanh')
-    network = tfl.dropout(network, dropout)
-    network = tfl.fully_connected(network, 256, activation='tanh')
-    network = tfl.dropout(network,dropout)
-    network = tfl.fully_connected(network, 10, activation='softmax')
-    network = tfl.regression(network, optimizer='adam', learning_rate=learning_rate,
+# Param : dropout value , learning rate , input shape
+def create_model(learning_rate, input_shape, nb_classes, base_path, drop=0.0):
+    network = input_data(shape=input_shape, name='input')
+    network = conv_2d(network, 64, [4, 16], activation='relu')
+    # network = max_pool_2d(network, 2)
+    # network = local_response_normalization(network)
+    # network = conv_2d(network, 64, [2, 8], activation='relu')
+    # network = max_pool_2d(network, 2)
+    # network = local_response_normalization(network)
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, drop)
+    network = fully_connected(network, 64, activation='relu')
+    network = dropout(network, drop)
+    network = fully_connected(network, nb_classes, activation='softmax')
+    network = regression(network, optimizer='sgd', learning_rate=learning_rate,
                          loss='categorical_crossentropy', name='target')
-
-    return network
+    model = tflearn.DNN(network, tensorboard_verbose=3, tensorboard_dir=base_path + "/tflearn_logs/",
+                        checkpoint_path=base_path + "/checkpoints/step")
+    return model
--- speech_cnn/train.py
+++ speech_cnn/train.py
@@ -2,62 +2,69 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import argparse
 import os.path
+
 import numpy as npy
-import tflearn as tfl
 import data_loader as data
 import models
+import time
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+dir = os.path.dirname(__file__)
 
 
-def main(_):
-    dropout = 0.5
-    learning_rate = 0.0005
+def init():
+    dropout = 0.6
+    learning_rate = 0.01
     snapshot_step = 500  # steps to snapshot
-    epoch = 500
-    batch_size = 100
-    runId = 'speech_cnn'
+    epoch = 200000
+    batch_size = 64
+    run_id = 'speech_cnn_' + str(int(time.time()))
 
-    width = 20  # mfcc features
+    # Inputs shaping
+    width = 20  # mfcc featu
     height = 80  # (max) length of utterance
     classes = 10  # digits
+    input_shape = [-1, width, height, 1]
 
-    batch = word_batch = data.mfcc_batch_generator(batch_size)
+    # Fetch inputs
+    batch = data.mfcc_batch_generator(batch_size)
     X, Y = next(batch)
-    X = tfl.reshape(X, [-1, 20, 80, 1])
+    X = npy.reshape(X, input_shape)
     testX, testY = next(batch)
-    testX = tfl.reshape(testX, [-1, 20, 80, 1])
+    testX = npy.reshape(testX, input_shape)
 
-    # Instantiante model
-    network = models.create_model(dropout, learning_rate)
+    # Instantiante model for test
+    # accX, accY = next(batch)
+    # accX = npy.hape(accX, input_shape)
+    # model_test = models.create_model(learning_rate, [None, width, height, 1], classes)
 
-    model = tfl.DNN(network, tensorboard_verbose=0)
-    evaluate_model_accuracy(model , next(batch) )
+    # Instantiante model for training
+    model = models.create_model(learning_rate, [None, width, height, 1], classes, dir, drop=dropout)
+
+    # evaluate_model_accuracy(model, testX, testY)
     model.fit({'input': X}, {'target': Y}, n_epoch=epoch, validation_set=({'input': testX}, {'target': testY}),
-              show_metric=True, batch_size=batch_size, shuffle=True,
-              snapshot_epoch=False, snapshot_step=snapshot_step,
-              validation_batch_size=batch_size, run_id=runId)
+              show_metric=True, snapshot_epoch=False, snapshot_step=snapshot_step, run_id=run_id)
 
 
 # Test model accuracry
 def evaluate_model_accuracy(model, X, Y):
-    good_res = 0
-    batch_size = X.len
+    good_ = 0
     i = 0
+    batch_size = len(X)
     while i < batch_size:
         # Run the model on one example
         prediction = model.predict([X[i]])
         label = npy.argmax(Y[i])
-        print("Real label : " + str(label))
         # print("Prediction: %s" % str(prediction[0]))
-        predRes = npy.argmax(prediction[0])
-        print("Prediction: %s" % str(predRes))
+        pred = npy.argmax(prediction[0])
         i = i + 1
-        if (label == predRes):
-            print("Reco OK !")
-            good_res = good_res + 1
-        print("=============")
-    print("---------------")
-    print("Reco avg: %s" % str(good_res / (batch_size)))
+        if (label == pred):
+            good_ = good_ + 1
+    print("Accuracy test batch size :" + str(batch_size))
+    print("Total good reco :" + str(good_))
+    print("Reco avg: %s " % str(round((good_ * 100) / (batch_size))) + "%")
+
+
+if __name__ == "__main__":
+    init()
